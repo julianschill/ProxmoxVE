6f9a52ae3c363e0df2593356f0b24e8237f00ca8
Julian Schill <j.schill@web.de>
2026-01-15 00:54:59 +0100

    Fix

--- ct/stash.sh
+++ ct/stash.sh
@@ -5,14 +5,15 @@ source <(curl -fsSL https://raw.githubusercontent.com/julianschill/ProxmoxVE/ref
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/stashapp/stash
 
+# Setting variables for build.func
 APP="Stash"
-var_tags="${var_tags:-media;organizer}"
-var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-2048}"
-var_disk="${var_disk:-8}"
-var_os="${var_os:-debian}"
-var_version="${var_version:-12}"
-var_unprivileged="${var_unprivileged:-1}"
+var_tags="media"
+var_cpu="2"
+var_ram="2048"
+var_disk="8"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
 
 header_info "$APP"
 variables
