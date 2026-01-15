6a56d04196570ac29e9a06125d58cf61f81b72a9
Julian Schill <j.schill@web.de>
2026-01-15 01:10:07 +0100

    Fix tags

--- ct/stash.sh
+++ ct/stash.sh
@@ -7,13 +7,13 @@ source <(curl -fsSL https://raw.githubusercontent.com/julianschill/ProxmoxVE/ref
 
 # Setting variables for build.func
 APP="Stash"
-var_tags="media"
-var_cpu="2"
-var_ram="2048"
-var_disk="8"
-var_os="debian"
-var_version="12"
-var_unprivileged="1"
+var_tags="${var_tags:-media}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-8}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
 
 header_info "$APP"
 variables
